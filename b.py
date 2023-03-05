c6fe408e0fad1a314645006252ce77b8bc807150
Chirag <76880977+chirag127@users.noreply.github.com>
2023-03-06 03:10:31 +0530

    Update and rename bi.py to b.py

--- b.py
+++ b.py
@@ -5,25 +5,7 @@ import re
 
 import requests
 
-
-def do_content_type_topics(j,ps_file, topic, content_type):
-    i = topic
-
-    topic = topic.replace("#", "")
-    if content_type == "all":
-        message = (
-            "give a comprehensive overview with definitions"
-            + f", examples, applications, benefits of {topic}"
-            + "  with definitions, examples, applications"
-            + " , benefits for Unit 1 - Introduction to Big Data."
-        )
-
-    elif content_type == "text":
-        message = f"write in detail about {topic}"
-
-    elif content_type == "image":
-        message = f"draw, give and show detailed image for {topic}"
-    elif content_type == "diagram":
+ == "diagram":
         message = f"draw and show me comprehensively detailed diagram with details for {topic}"
 
     elif content_type == "explain":
@@ -55,43 +37,10 @@ def do_content_type_topics(j,ps_file, topic, content_type):
 
     print(message)
 
-    if len(topic) > 100:
-        topic = topic[:100]
-
-    topic = topic.replace(" ", "_")
-
-    topic = topic.replace("/", "_")
-
-    topic = topic.replace(":", "_")
-
-    topic = topic.replace("?", "_")
-
-    topic = topic.replace("!", "_")
-
-    topic = topic.replace(",", "_")
-
-    topic = topic.replace(".", "_")
-
-    topic = topic.replace("(", "_")
-
-    topic = topic.replace(")", "_")
-
-    file_name = f"{topic}.md"
+    if len
 
 
-
-    file_name = f"{j:03d}_" + file_name
-
-    print(file_name)
-
-    file_path = f"notes/bing/{content_type}/{ps_file.replace('.txt','')}/{file_name}"
-
-    print(file_path)
-
-    if not os.path.exists(os.path.dirname(file_path)):
-        os.makedirs(os.path.dirname(file_path))
-
-    if not os.path.exists(file_path):
+if not os.path.exists(file_path):
         try:
             with open(file_path, "w", encoding="utf8") as file:
                 file.write(f"{i.split('for the notes')[0]}\n\n")
@@ -130,16 +79,7 @@ def do_content_type_topics(j,ps_file, topic, content_type):
             text = text.splitlines()[:-1]
             text = "\n".join(text)
 
-        if not os.path.exists(os.path.dirname(file_path)):
-            os.makedirs(os.path.dirname(file_path))
-
-        with open(file_path, "a", encoding="utf8") as file:
-            file.write(f"{text}")
-
-
-def main(content_type="text"):
-
-    print(os.getcwd())
+(os.getcwd())
 
     ps_files = glob.glob(f"p_s/**/*.txt")
 
@@ -175,52 +115,3 @@ def main(content_type="text"):
 
 
 
-if __name__ == "__main__":
-
-
-    def main_1():
-        from remove_empty_notes import remove
-
-        remove()
-
-        types = [
-            "all",
-            "text",
-            "image",
-            "diagram",
-            "explain",
-            "ab",
-            "ch",
-            "code",
-            "example",
-            "definition",
-            "vr",
-            "summary",
-        ]
-
-
-        import concurrent.futures
-
-
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(types)) as executor:
-            executor.map(main, types)
-    k = 1000
-
-    while k > 0:
-
-        try:
-            main_1()
-
-            k -= 1
-
-        except Exception as e:
-
-            print(e)
-
-            with open("error.txt", "a", encoding="utf8") as file:
-                file.write(f"{e}\n\n")
-
-            k -= 1
-
-    # main()
